4f1cf20bc1f139225e516fc0738e8235712d8388
vkuehn <vkuehn@more-machines.org>
2019-10-19 04:55:48 +0100

    start working on bno055

---
+++ python/tornado/04_bno055/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -d ~/fpvcar ]; then
+  echo "create fpvcar folders"
+  	mkdir -p ~/fpvcar/venv
+  echo "default config"
+        cp config.json ~/fpvcar
+  echo "create python3 venv"
+        sudo apt-get install python3-venv
+  	python3 -m venv ~/fpvcar/venv
+fi
