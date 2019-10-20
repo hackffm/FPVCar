79b28c9ac21f370e931158291a770ec671d21e4e
vkuehn <vkuehn@more-machines.org>
2019-10-20 12:04:50 +0200

    move helper functions to component

---
+++ python/tornado/04_bno055/components/helper.py
@@ -0,0 +1,48 @@
+import os
+import socket
+
+import netifaces
+
+
+class Helper:
+
+    def __init__(self):
+        pass
+
+    def infos_self(self):
+        infos = []
+        infos.append('hostname ' + str(socket.gethostname()))
+        infos.append('PID ' + str(os.getpid()))
+        ifaces = self.interfaces_self()
+        for iface in ifaces:
+            infos.append(iface)
+        return infos
+
+    def interfaces_first(self):
+        ips = self.interfaces_self()
+        # remove ipv6 from results
+        for ip in ips:
+            if ':' not in ip:
+                return ip
+        return '127.0.0.1'
+
+    def interfaces_self(self):
+        ifaces = []
+        for interface in netifaces.interfaces():
+            if interface != 'lo':
+                if 2 in netifaces.ifaddresses(interface):
+                    _i = netifaces.ifaddresses(interface)
+                    _i = _i[2][0]['addr']
+                    if self.not_local(_i):
+                        ifaces.append(_i)
+                if 17 in netifaces.ifaddresses(interface):
+                    _i = netifaces.ifaddresses(interface)
+                    _i = _i[17][0]['addr']
+                    if self.not_local(_i):
+                        ifaces.append(_i)
+                if 18 in netifaces.ifaddresses(interface):
+                    _i = netifaces.ifaddresses(interface)
+                    _i = _i[18][0]['addr']
+                    if self.not_local(_i):
+                        ifaces.append(_i)
+        return ifaces
