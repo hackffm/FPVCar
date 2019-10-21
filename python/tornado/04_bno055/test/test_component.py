d17946826ce25360241e696d8d458fad98f6ec54
vkuehn <vkuehn@more-machines.org>
2019-10-21 03:15:52 +0200

    change to new message format

--- python/tornado/04_bno055/test/test_component.py
+++ python/tornado/04_bno055/test/test_component.py
@@ -4,7 +4,7 @@ from components import *
 
 sensor = Bno055()
 
-print(sensor.handleMessage('acceleration'))
-print(sensor.handleMessage('temperature'))
-print(sensor.handleMessage('all'))
-assert sensor.handleMessage('nono') == 'failed', 'Failed handling unknown Sensor request'
+print(sensor.handleMessage({'sensor': 'acceleration'}))
+print(sensor.handleMessage({'sensor': 'temperature'}))
+print(sensor.handleMessage({'sensor': 'all'}))
+assert sensor.handleMessage({'sensor': 'nono'}) == 'failed', 'Failed handling unknown Sensor request'
