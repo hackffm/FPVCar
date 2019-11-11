44d3c9ca8ddcd537450a47e64a55b4654659a379
vkuehn <vkuehn@more-machines.org>
2019-11-11 17:18:54 +0100

    add test for serial connection

---
+++ python/tornado/04_bno055/test/test_serial.py
@@ -0,0 +1,32 @@
+import serial
+
+ser = serial.Serial('/dev/ttyS0', 38400)
+print(ser.name)
+
+
+def serial_read():
+    print('serial_read')
+    data = b''
+    for i in range(ser.inWaiting()):
+        b = ser.read(1)
+        if b != b'\r':
+            if b == b'\n':
+                print('msg from arduino: ', data)
+                print('msg decoded' + data.decode("utf-8"))
+                data = b''
+            else:
+                data += b
+
+
+if __name__ == '__main__':
+    ser.flushInput()
+
+    cmd = "V\r"
+    print("fetch: " + cmd)
+    ser.write(cmd.encode())
+    serial_read()
+
+    cmd = "v\r"
+    print("fetch: " + cmd)
+    ser.write(cmd.encode())
+    serial_read()
