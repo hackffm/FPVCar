8b9be716f01d5c22a3a7b21ec8126ac39687c265
vkuehn <vkuehn@more-machines.org>
2020-02-20 19:41:57 +0100

    typo

--- python/tornado/06_serial_adapter/test/test_used_serialports.py
+++ python/tornado/06_serial_adapter/test/test_used_serialports.py
@@ -5,5 +5,5 @@ ports = list(serial.tools.list_ports.comports())
 if len(ports) == 0:
     print('no used ports found')
 for p in ports:
-    print('thing is ' + p.description + ' on port ' + p.device)
+    print('port is ' + p.description + ' on port ' + p.device)
 
